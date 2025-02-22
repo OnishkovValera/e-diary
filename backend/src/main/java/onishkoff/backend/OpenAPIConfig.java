df0d96f7a17f9c1eeec769557a00a98995a4ab1b
valera <67824716+OnishkovValera@users.noreply.github.com>
2025-02-23 02:04:25 +0300

    Testing and debugging openapi generation(it takes 4 hours)

---
+++ backend/src/main/java/onishkoff/backend/OpenAPIConfig.java
@@ -0,0 +1,25 @@
+package onishkoff.backend;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+import java.util.List;
+
+@Configuration
+public class OpenAPIConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI().servers(
+                List.of(
+                        new Server().url("https://localhost:8080")
+                )
+        ).info(
+                new Info().title("API Documentation").version("1.0")
+        );
+    }
+}
