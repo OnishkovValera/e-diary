2c1284873f1f604342eac9dceb5c3a8e49c3a5bc
valera <67824716+OnishkovValera@users.noreply.github.com>
2025-02-22 19:51:02 +0300

    Backend initialization

---
+++ backend/src/test/java/onishkoff/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package onishkoff.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
